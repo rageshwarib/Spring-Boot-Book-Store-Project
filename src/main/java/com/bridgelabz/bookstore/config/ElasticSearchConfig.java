8bffe02e19b00124c650bcdd0c93dd50963771f2
Rageshwari Bodke <ragu.bodke@gmail.com>
2020-06-27 17:37:44 +0530

    REFACTORED: Code that passes Book model to pagination instead of Book Dto and also changes the book id type from int to long in wishlist model class

---
+++ src/main/java/com/bridgelabz/bookstore/config/ElasticSearchConfig.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.bookstore.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+	@Value("${elasticsearch.host}")
+    private String elasticsearchHost;
+
+    @Bean(destroyMethod = "close")
+    public RestHighLevelClient client() {
+
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost(elasticsearchHost)));
+
+        return client;
+
+    }
+
+}
